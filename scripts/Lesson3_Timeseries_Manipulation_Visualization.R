1d9c0ada3d2afa4824c4eaa72f36e689f7c7f3d6
wwoelmer <wwoelmer@vt.edu>
2025-05-16 11:55:00 +1200

    draft ideas for lesson 3

---
+++ scripts/Lesson3_Timeseries_Manipulation_Visualization.R
@@ -0,0 +1,29 @@
+# Lesson 2
+
+###########################################
+##### summarise data down to monthly and combine
+# read in buoy data (15 minutes)
+  # summarise to daily
+
+# combine with CTD data (monthly)
+
+# pivot longer
+
+# plot CTD on x, buoy on y for a given variable
+
+# create year variable using mutate
+
+# color by year
+
+###############################################
+##### use ceiling/floor or padR tools to combine sub-daily datasets
+# combine buoy 15 minute data with something that is hourly (met data? something else relevant to BOPRC)
+
+
+###############################################
+### lag analysis
+# create AR1 lag
+  # create PACF and ACF plots
+
+##############################################
+## trend analysis? or put into statistical lesson
